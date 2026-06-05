afe56920599d15fae961cab0d8572f3442ca3e9c
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2026-06-04 22:21:22 -0500

    Added a check to open configuration GUI depending if the player isnt holding anything. https://github.com/mas6y6/ConfigureableCrushingWheel/issues/1

--- src/main/java/com/mas6y6/configureablecrushingwheel/server/ConfigureablecrushingwheelServer.java
+++ src/main/java/com/mas6y6/configureablecrushingwheel/server/ConfigureablecrushingwheelServer.java
@@ -2,8 +2,8 @@ package com.mas6y6.configureablecrushingwheel.server;
 
 import com.mas6y6.configureablecrushingwheel.common.IConfiguredCrushingWheelUUID;
 import com.mas6y6.configureablecrushingwheel.common.IConfiguredMillstoneUUID;
-import com.mas6y6.configureablecrushingwheel.common.packets.OpenCrushingWheelRecipeGuiPacket;
-import com.mas6y6.configureablecrushingwheel.common.packets.OpenMillstoneRecipeGuiPacket;
+import com.mas6y6.configureablecrushingwheel.common.packets.ClientBoundOpenCrushingWheelRecipeGuiPacket;
+import com.mas6y6.configureablecrushingwheel.common.packets.ClientBoundOpenMillstoneRecipeGuiPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -33,18 +33,18 @@ public class ConfigureablecrushingwheelServer {
 
     @SubscribeEvent
     public void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
-
         if (event.getLevel().isClientSide()) return;
         if (event.getHand() != InteractionHand.MAIN_HAND) return;
+        if (!event.getEntity().getMainHandItem().isEmpty()) return;
 
         BlockEntity blockentity = event.getLevel().getBlockEntity(event.getPos());
 
         if (blockentity instanceof IConfiguredCrushingWheelUUID configuredCrushingWheel) {
             UUID uuid = configuredCrushingWheel.getUUID();
-            PacketDistributor.sendToPlayer((ServerPlayer) event.getEntity(),new OpenCrushingWheelRecipeGuiPacket(uuid.toString()));
+            PacketDistributor.sendToPlayer((ServerPlayer) event.getEntity(),new ClientBoundOpenCrushingWheelRecipeGuiPacket(uuid.toString()));
         } else if (blockentity instanceof IConfiguredMillstoneUUID millstone) {
             UUID uuid = millstone.getUUID();
-            PacketDistributor.sendToPlayer((ServerPlayer) event.getEntity(),new OpenMillstoneRecipeGuiPacket(uuid.toString()));
+            PacketDistributor.sendToPlayer((ServerPlayer) event.getEntity(),new ClientBoundOpenMillstoneRecipeGuiPacket(uuid.toString()));
         }
     }
 }
